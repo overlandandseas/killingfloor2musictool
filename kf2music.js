4d0d6cbd80c63d3be99d3f366dc0a2a121e88196
Lucas Overland <loverland@cars.com>
2015-12-25 08:37:46 -0600

    gui up and running

---
+++ kf2music.js
@@ -0,0 +1,29 @@
+//JavaScript
+var angular = require('angular');
+var fs = require('fs');
+
+var Song = function(inTitle, inArtist, inFilename ) {
+    this.title = inTitle;
+    this.artist = inArtist;
+    this.fileName = inFilename; //wemcode
+    // this.path = inPath;
+};
+
+Song.prototype.toString = function () {
+    return this.artist + " - " + this.title;
+};
+
+Song.randomSong = function() {
+    return new Song ("Song Title" + getRandomInt(0, 10), "DH_" + getRandomInt(0, 20), getRandomInt(0, 99999) + ".wem");
+}
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+var data = [Song.randomSong(), Song.randomSong(), Song.randomSong(), Song.randomSong(), Song.randomSong()];
+
+angular.module('kf2Music', []).controller('kf2MusicController', function($scope) {
+    $scope.title = "Killing Floor 2 Music Tool";
+    $scope.songs = data;
+});
