d5866a23a64056ffb21759bf73c82225d5887460
Lucas Overland <lucas.overland@gmail.com>
2016-02-02 00:12:54 -0600

    front end change to sniff for known paths

--- KfStreamedAudio.js
+++ KfStreamedAudio.js
@@ -8,79 +8,88 @@ const targz = require('targz');
 
 class KfStreamedAudio {
 
-    constructor(inId, inName, inAudoSourceFile, inGeneratedAudioFile, inWiseObjectPath, inNotes) {
-        this.id = inId;
-        this.name = inName;
-        this.audioSourceFile = inAudoSourceFile;
+  constructor(inId, inName, inAudoSourceFile, inGeneratedAudioFile, inWiseObjectPath, inNotes) {
+    this.id = inId;
+    this.name = inName;
+    this.audioSourceFile = inAudoSourceFile;
 
-        //probably won't need anything past this but might as well if it's free.
-        this.generatedAudioFile = inGeneratedAudioFile;
-        this.wiseObjectPath = inWiseObjectPath;
-        this.notes = inNotes;
+    //probably won't need anything past this but might as well if it's free.
+    this.generatedAudioFile = inGeneratedAudioFile;
+    this.wiseObjectPath = inWiseObjectPath;
+    this.notes = inNotes;
+  }
+
+  //static methods
+
+  //create from JSON array
+  static generateSongsFromArray(inArr) {
+    let kfArr = [];
+
+    for(let index in inArr) {
+      kfArr.push(this.object2KfStreamAudio(inArr[index]));
     }
+    return kfArr;
+  }
 
-    //static methods
+  //create KfStreamedAduio from object
+  static object2KfStreamAudio(inObj) {
+    return new KfStreamedAudio(inObj["ID"], inObj["Name"], inObj["Audio source file"], inObj["Generated audio file"], inObj["Wwise Object Path"], inObj["Notes"]);
+  }
 
-    //create from JSON array
-    static generateSongsFromArray(inArr) {
-        let kfArr = [];
+  //setter for static data member wwiseCliPath
+  static setWwiseCliPath(pathName) {
+    this.wwiseCliPath = pathName;
+  }
+  static getWwiseCliPath() {
+    return this.wwiseCliPath;
+  }
 
-        for(let index in inArr) {
-            kfArr.push(this.object2KfStreamAudio(inArr[index]));
-        }
-        return kfArr;
+  static setKfPath(pathName) {
+    this.kfPath = pathName;
+  }
+  static getKfPath() {
+    return this.kfPath;
+  }
+
+  static searchForDefaultKfPath() {
+    if(fs.statSync("C:\\Program Files (x86)\\Steam\\steamapps\\common\\killingfloor2\\KFGame").isDirectory()) {
+      this.setKfPath("C:\\Program Files (x86)\\Steam\\steamapps\\common\\killingfloor2\\KFGame\\BrewedPC\\WwiseAudio\\Windows\\English(US)")
+      return true
+    } else {
+      return false
     }
+  }
 
-    //create KfStreamedAduio from object
-    static object2KfStreamAudio(inObj) {
-        return new KfStreamedAudio(inObj["ID"], inObj["Name"], inObj["Audio source file"], inObj["Generated audio file"], inObj["Wwise Object Path"], inObj["Notes"]);
+  static searchForDefaultWwisePath() {
+    if(fs.statSync("C:\\Program Files (x86)\\Audiokinetic\\Wwise v2015.1.4 build 5497\\Authoring\\Win32\\Release\\bin\\WwiseCLI.exe").isFile()) {
+      this.setWwiseCliPath("C:\\Program Files (x86)\\Audiokinetic\\Wwise v2015.1.4 build 5497\\Authoring\\Win32\\Release\\bin\\WwiseCLI.exe")
+      return true
+    } else {
+      return false
     }
+  }
 
-    //setter for static data member wwiseCliPath
-    static setWwiseCliPath(pathName) {
-        this.wwiseCliPath = pathName;
-    }
-    static getWwiseCliPath() {
-        return this.wwiseCliPath;
-    }
+  //@TODO make sure this is actually working.
 
-    static setKfPath(pathName) {
-        this.kfPath = pathName;
-    }
-    static getKfPath() {
-        return this.kfPath;
-    }
+  //private methods
+  static _unzipWwiseTemplate(cb) {
 
-    //@TODO make sure this is actually working.
+  }
 
-    //private methods
-    static _unzipWwiseTemplate(cb) {
-        
-    }
+  //non-static methods
+  toString() {
+    return this.name;
+  }
 
-    //non-static methods
-    toString() {
-        return this.name;
-    }
-
-    isChanged() {
-        return !!this.wavName;
-    }
+  isChanged() {
+    return !!this.wavName;
+  }
 
 
-    //the big one (not at the moment)
-    swapAudioSource(inName, inPath) {
-
-        this.wavName = inName;
-        //lets do this but BLOCK the fucking thread cause you are going to not want to do shit.
-        fs.renameSync(inPath, `${__dirname}\\assets\\Wwise_Template_Migration\\Originals\\SFX\\song.wav`);
-        //update progress bar
-        cp.execSync(`${KfStreamedAudio.wwiseCliPath} ${__dirname}\\assets\\Wwise_Template_Migration\\Template.wproj -GenerateSoundBanks -Platform Windows`);
-        //update progress bar
-        fs.renameSync(`${__dirname}\\assets\\Wwise_Template_Migration\\.cache\\windowssong.wav`, `${KfStreamedAudio.pathToKf}\\Music\\${this.audioSourceFile}`);
-        //update user
-
-    }
+  //the big one (not at the moment)
+  swapAudioSource(inName, inPath) {
+    //@TODO Everything...
+  }
 
 }
 
--- kf2music.js
+++ kf2music.js
@@ -7,27 +7,33 @@ const KfStreamedAudio = require('./KfStreamedAudio');
 
 
 angular.module('kf2Music', []).controller('kf2MusicController', function($scope) {
-    $scope.title = "Killing Floor 2 Music Tool";
-    // $scope.songs = Song.generateSongs([], 50);
-    let songArr = JSON.parse(fs.readFileSync(__dirname + '/data/nah.json'));
-    let validSongArr = JSON.parse(fs.readFileSync(__dirname + '/data/initialdata.json'));
+  $scope.title = "Killing Floor 2 Music Tool";
+  // $scope.songs = Song.generateSongs([], 50);
+  let songArr = JSON.parse(fs.readFileSync(__dirname + '/data/nah.json'));
+  let validSongArr = JSON.parse(fs.readFileSync(__dirname + '/data/initialdata.json'));
 
-    console.log(validSongArr);
-    $scope.songs = KfStreamedAudio.generateSongsFromArray(validSongArr);
 
-    KfStreamedAudio.setKfPath("C:\\Program Files (x86)\\Steam\\steamapps\\common\\killingfloor2\\KFGame");
-    KfStreamedAudio.setWwiseCliPath("C:\\Program Files (x86)\\Audiokinetic\\Wwise v2015.1.4 build 5497\\Authoring\\Win32\\Release\\bin\\WwiseCLI.exe");
+  $scope.kfPathFound = KfStreamedAudio.searchForDefaultKfPath()
+  $scope.wwisePathFound = KfStreamedAudio.searchForDefaultWwisePath()
 
-    $scope.killMe = function () {
-        fs.writeFile('data/initalData.json', JSON.stringify(this.songs, null, 4), function () {
-            $scope.h3Tag = "File Has been Saved";
-        });
-    };
 
-    $scope.fileChanges = function (index, input) {
-        console.log(input.files[0].path);
-        $scope.songs[index].swapAudioSource(input.files[0].name, input.files[0].path);
-        $scope.$apply();
-    };
+  console.log(validSongArr);
+  $scope.songs = KfStreamedAudio.generateSongsFromArray(validSongArr);
+
+  // KfStreamedAudio.setKfPath("C:\\Program Files (x86)\\Steam\\steamapps\\common\\killingfloor2\\KFGame");
+  // KfStreamedAudio.setWwiseCliPath("C:\\Program Files (x86)\\Audiokinetic\\Wwise v2015.1.4 build 5497\\Authoring\\Win32\\Release\\bin\\WwiseCLI.exe");
+
+  $scope.killMe = function () {
+    fs.writeFile('data/initalData.json', JSON.stringify(this.songs, null, 4), function () {
+      $scope.h3Tag = "File Has been Saved";
+    });
+  };
+
+  $scope.fileChanges = function (index, input) {
+    console.log(input.files[0].path);
+    $scope.songs[index].swapAudioSource(input.files[0].name, input.files[0].path);
+    $scope.$apply();
+  };
+
 
 });
