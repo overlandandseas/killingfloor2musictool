966f1c7da9f251af9ed4527ea22edcb54e9d187a
Lucas Overland <lucas.overland@gmail.com>
2016-02-07 02:28:39 -0600

    wwise object updates

--- script.js
+++ script.js
@@ -3,17 +3,27 @@
 var angular = require('angular')
 const path = require('path')
 const KFGame = require('./lib/KFGame')
+const WwiseUtilities = require('./lib/WwiseUtilities')
 
 angular.module('projectDemonHunterHunter', []).controller('kf2MusicController', function ($scope) {
   // developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions#Lexical_this
   let kfGame = new KFGame()
+  let wwiser = new WwiseUtilities()
 
-  $scope.kfPath = kfGame.PATH
+  $scope.kfPath = kfGame.GAMEPATH
   $scope.songs = kfGame.wems
+  $scope.wwPath = wwiser.EXEPATH
 
   $scope.getkfpath = function (input) {
     kfGame.checkPath(input.files[0].path)
-    $scope.kfPath = `${path.dirname(kfGame.GAMEPATH)}\\KFGame.exe`
+    $scope.kfPath = kfGame.GAMEPATH ? `${path.dirname(kfGame.GAMEPATH)}\\KFGame.exe` : null
     $scope.$apply()
   }
+
+  $scope.getwwpath(input) {
+    wwiser.checkPath(input.files[0].path)
+    $scope.wwPath = wwise.EXEPATH ? `${path.dirname(wwiser.EXEPATH)}\\Wwise.exe` : null
+    $scope.$apply()
+  }
+
 })
